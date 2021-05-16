775391b7b9b65a88eecbf1d25daf39b213a17be9
Madiyor <m.abdukhashimov@gmail.com>
2021-05-16 14:24:38 +0500

    feat: main index file update - needs to work

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
+import 'reflect-metadata'
+// import app from ''
+// import databaseConfig from './config/databaseConfig'
+import app from './app'
+import DB from './core/db'
+if (!process.env) {
+    throw new Error(`.env file required pleas create`)
+}
+const PORT = process.env.PORT || 3000
+
+let existingDB: DB | null = null
 ;(async function () {
     try {
         const db = new DB()

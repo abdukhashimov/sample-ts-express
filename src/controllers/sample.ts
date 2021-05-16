2e068ec7b484de238336ac9056bc0bc2785cc3ca
Madiyor <m.abdukhashimov@gmail.com>
2021-05-16 12:14:50 +0500

    feat: add new controller

---
+++ src/controllers/sample.ts
@@ -0,0 +1,13 @@
+import { NextFunction } from "express"
+
+export class SampleController {
+    async create(req: Request, res: Response, next: NextFunction) {}
+
+    async update(req: Request, res: Response, next: NextFunction) {}
+
+    async find(req: Request, res: Response, next: NextFunction) {}
+
+    async get(req: Request, res:Response, next:NextFunction) {}
+
+    async delete(req: Request, res: Response, next: NextFunction) {}
+}

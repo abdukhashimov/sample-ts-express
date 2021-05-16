bb64630b3a19a85f8c43c69e6607b1fc7ad19467
Madiyor <m.abdukhashimov@gmail.com>
2021-05-16 12:27:58 +0500

    feat: add router and pass functions to it

--- src/controllers/sample.ts
+++ src/controllers/sample.ts
@@ -1,11 +1,14 @@
-import { NextFunction } from "express"
+import { NextFunction, Request, Response } from "express"
 
 export class SampleController {
+
     async create(req: Request, res: Response, next: NextFunction) {}
 
     async update(req: Request, res: Response, next: NextFunction) {}
 
-    async find(req: Request, res: Response, next: NextFunction) {}
+    async find(req: Request, res: Response, next: NextFunction) {
+        throw new Error("not implemented yet")
+    }
 
     async get(req: Request, res:Response, next:NextFunction) {}
 
---
+++ src/routes/sample.ts
@@ -0,0 +1,14 @@
+import { Router } from "express"
+import { SampleController } from "../controllers/sample"
+
+const router = Router({ mergeParams: true })
+const controller = new SampleController()
+
+router.route("/").get(controller.find).post(controller.create)
+
+router
+    .route("/:id")
+    .get(controller.get)
+    .put(controller.update)
+    .patch(controller.update)
+    .delete(controller.delete)
